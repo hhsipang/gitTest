24052567449ed2585124bb2995c86b85ae7c623a
hhsipang <66022285+hhsipang@users.noreply.github.com>
2020-05-28 10:42:43 +0800

    在线编辑

--- src/main/java/com/github/HelloGit.java
+++ src/main/java/com/github/HelloGit.java
@@ -5,6 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("gegnxin1");
+		System.out.println("gegnxin2");
+		System.out.println("gegnxin3");
+		System.out.println("gegnxin4");
 	}
 
 }
